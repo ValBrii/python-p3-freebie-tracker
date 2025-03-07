1efcea222a78e3d38f9485d2e076bdb6f212b189
Valerie <britneyvalerie85@gmail.com>
2025-03-07 10:38:27 +0300

    changes added

--- lib/models.py
+++ lib/models.py
@@ -2,7 +2,6 @@ from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
@@ -16,43 +15,22 @@ class Company(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
-
-    
-    freebies = relationship("Freebie", backref="company")
-    devs = relationship("Dev", secondary="freebies", backref="companies")
+    freebies = relationship("Freebie", back_populates="company")
+    devs = relationship("Dev", secondary="freebies", back_populates="companies")
 
     def __repr__(self):
         return f'<Company {self.name}>'
-
-    def give_freebie(self, dev, item_name, value):     
-        freebie = Freebie(item_name=item_name, value=value, dev_id=dev.id, company_id=self.id)
-        return freebie
-
-    @classmethod
-    def oldest_company(cls, session):
-       
-        return session.query(cls).order_by(cls.founding_year).first()
-
 
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
-
-
-    freebies = relationship("Freebie", backref="dev")
+    freebies = relationship("Freebie", back_populates="dev")   
+    companies = relationship("Company", secondary="freebies", back_populates="devs")
 
     def __repr__(self):
         return f'<Dev {self.name}>'
-
-    def received_one(self, item_name):      
-        return any(freebie.item_name == item_name for freebie in self.freebies)
-
-    def give_away(self, dev, freebie):      
-        if freebie.dev == self:
-            freebie.dev = dev
-
 
 class Freebie(Base):
     __tablename__ = 'freebies'
@@ -62,9 +40,14 @@ class Freebie(Base):
     value = Column(Integer())
     dev_id = Column(Integer(), ForeignKey('devs.id'))
     company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev = relationship("Dev", back_populates="freebies")
+    company = relationship("Company", back_populates="freebies")
 
     def __repr__(self):
-        return f'<Freebie {self.item_name} (Value: {self.value})>'
+        return f'<Freebie {self.item_name}>'
 
-    def print_details(self):
-        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
+    
+    
+    
+
+
